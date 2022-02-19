9ca7618e277a401640ed773c49d136f7532e9ada
ahmedbesbes <ahmed.besbes@hotmail.com>
2022-02-19 10:49:14 +0100

    Add youtube video thumbnail and clean url

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
+import re
 import streamlit as st
+from streamlit_player import st_player
 from youtube_transcriber import YoutubeTranscriber
 
 st.set_page_config(layout="wide")
+
+
+def clean_video_url(video_url):
+    video_url = re.sub(f"&.*", "", video_url)
+    return video_url
 
 
 @st.cache(allow_output_mutation=True, suppress_st_warning=True)
@@ -46,6 +53,14 @@ def transcribe_video(
 
 
 video_url = st.sidebar.text_input("Enter youtube video")
+video_url = clean_video_url(video_url)
+
+
+columns = st.columns((1, 2, 1))
+with columns[1]:
+    st_player(video_url, height=500)
+
+
 if video_url.strip() != "":
     use_content_moderation = st.sidebar.checkbox("Use content moderation", value=False)
     use_topic_detection = st.sidebar.checkbox("Use topic detection", value=False)
